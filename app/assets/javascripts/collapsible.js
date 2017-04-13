6b4547a777c4698c1e94c0f1adb229de83cf6c01
ZeTopHat <colinhamilton579@gmail.com>
2017-04-12 23:50:20 -0600

    Major CSS facelift using bulma.io. Also added more javascript functionality and cookies.

---
+++ app/assets/javascripts/collapsible.js
@@ -0,0 +1,68 @@
+// Function to allow for collapsible content
+function collapsible(idHash) {
+	$.each(idHash, function(key, value) {
+		var elements = $(document.getElementById(key)).next().children();
+		for (var i = 0; i < elements.length; i++) {
+			var child = elements[i];
+			if(value){
+				child.style.display = 'inherit';
+			} else {
+				child.style.display = 'none';
+			}
+		}
+		if (!value){
+			$(document.getElementById(key)).append($('<sup class="supersup">collapsed</sup>'));
+		} else {
+			$(document.getElementById(key)).find('sup').replaceWith('');
+		}
+	});
+}
+
+// Jquery to impliment collapse on proper click and with cookie
+document.addEventListener("page:change", function() {
+    if($('#collapsible').length){
+			idHash = {};
+			ids = [];
+			$(".collapsible").each(function() {
+				id = $(this).attr('id');
+				ids.push(id);
+			});
+			for (var i = 0; i < ids.length; i++) {
+				var collapsibleIdCookie = readCookie("collapsibleId" + ids[i]);
+				var expansionCookie = readCookie("expansion" + ids[i]);
+				// convert cookie text back to boolean
+				if (expansionCookie == "false"){
+      		expansion = false;
+    		} else {
+      		expansion = true;
+    		}
+				if (collapsibleIdCookie){
+					idHash[collapsibleIdCookie] = expansion;
+				}
+			}
+			// make sure cookie exists then run through function with cookies
+			if (idHash){
+				collapsible(idHash);
+			}
+
+    	$(".collapsible").click( function(){
+				idHash = {};
+				ids = [];
+      	id = $(this).attr('id');
+				ids.push(id);
+    		// setting cookies and toggling expansion
+      	document.cookie = "collapsibleId" + id + "=" + id + ";";
+				var expansionCookie = readCookie("expansion" + id);
+				if (expansionCookie == "false"){
+					expansion = false;
+				} else {
+					expansion = true;
+				}
+				expansion = !expansion;
+				document.cookie = "expansion" + id + "=" + expansion + ";";
+				idHash[id] = expansion;
+				collapsible(idHash);
+    	});
+    }
+});
+
