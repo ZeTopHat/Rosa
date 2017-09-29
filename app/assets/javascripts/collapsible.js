de3c1b6dd45ac152b9342ad5c18a551f76336412
Erick Diaz <erick@yours.co>
2017-09-28 21:01:23 -0600

    Refactoring collapsible

--- app/assets/javascripts/collapsible.js
+++ app/assets/javascripts/collapsible.js
@@ -1,67 +1,55 @@
 // Function to allow for collapsible content
 function collapsible(idHash) {
-	$.each(idHash, function(key, value) {
-		var elements = $(document.getElementById(key)).next().children();
-		for (var i = 0; i < elements.length; i++) {
-			var child = elements[i];
-			if(value){
-				child.style.display = '';
-			} else {
-				child.style.display = 'none';
-			}
-		}
-		if (!value){
-			$(document.getElementById(key)).append($('<sup class="supersup">collapsed</sup>'));
-		} else {
-			$(document.getElementById(key)).find('sup').replaceWith('');
-		}
+	$.each(idHash, (key, value) => {
+		let elements = $("#key").next().children();
+
+        // Use 'map' to iterate over elements instead of for-loop and use ternaries to assign conditional values
+        elements.map((element) => {
+            value ? element.style.display = '' : element.style.display = 'none'
+        });
+
+        value ? $("#key").find('sup').replaceWith('') : $("#key").append($('<sup class="supersup">collapses</sup>'));
 	});
+}
+
+// Parses some value; if its "false" (string) or false (boolean) returns false
+function parseBool(stringVal) {
+    return !(stringVal === false || stringVal === "false")
 }
 
 // Jquery to impliment collapse on proper click and with cookie
 document.addEventListener("page:change", function() {
 	if($('#collapsible').length){
-		idHash = {};
-		ids = [];
+		let idHash = {};
+		let ids = [];
+        let id;
+
 		$(".collapsible").each(function() {
 			id = $(this).attr('id');
 			ids.push(id);
 		});
-		for (var i = 0; i < ids.length; i++) {
-			var collapsibleIdCookie = readCookie("collapsibleId" + ids[i]);
-			var expansionCookie = readCookie("expansion" + ids[i]);
-			// convert cookie text back to boolean
-			if (expansionCookie == "false"){
-				expansion = false;
-			} else {
-				expansion = true;
-			}
-			if (collapsibleIdCookie){
-				idHash[collapsibleIdCookie] = expansion;
-			}
-		}
-		// make sure cookie exists then run through function with cookies
-		if (idHash){
-			collapsible(idHash);
-		}
+
+        ids.map((singleId) => {
+            let collapsibleIdCookie;
+            let expansion = parseBool(readCookie(`expansion${singleId}`));
+            if (collapsibleIdCookie = readCookie(`collapsibleId${singleId}`))  idHash[collapsibleIdCookie] = expansion; 
+        });
+        
+        if (idHash) collapsible(idHash);
 
 		$(".collapsible").click( function(){
 			idHash = {};
 			ids = [];
 			id = $(this).attr('id');
 			ids.push(id);
+
 			// setting cookies and toggling expansion
-			document.cookie = "collapsibleId" + id + "=" + id + ";";
-			var expansionCookie = readCookie("expansion" + id);
-			if (expansionCookie == "false"){
-				expansion = false;
-			} else {
-				expansion = true;
-			}
-				expansion = !expansion;
-				document.cookie = "expansion" + id + "=" + expansion + ";";
-				idHash[id] = expansion;
-				collapsible(idHash);
+			document.cookie = `collapsibleId${id}=${id};`;
+			expansion != parseBool(readCookie(`expansion ${id}`));
+
+			document.cookie = `expansion${id}=${expansion};`;
+			idHash[id] = expansion;
+			collapsible(idHash);
 		});
 	}
 });
